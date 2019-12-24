7c106d3bdf2c125433d9536064bf704f9d601a84
ForsakeLJK <forsakeljk@gmail.com>
2019-12-24 19:18:25 +0800

    实现了run和pi

--- Main.java
+++ Main.java
@@ -120,12 +120,17 @@ public class Main {
 	print (f 2 * 4)
 	print (2*(2+3)/4)	
 	*/ 
-	  
+	
+	/*
+	 * make "run [ [a] [repeat 1 :a] ] make "pi 3.14159
+	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		DataSpace space = new DataSpace(); // global space 
 		Parser parser = new Parser(space, space);  // main parser, global == local
 		
+		parser.parse("make \"run [ [a] [repeat 1 :a] ] make \"pi 3.14159", in);
+
 		while(in.hasNextLine()) {
 			String str = in.nextLine() + " "; 
 			parser.parse(str, in);
