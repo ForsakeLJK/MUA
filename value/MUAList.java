181732b7e5700d0149dc02215edb0f4fbcab5365
ForsakeLJK <forsakeljk@gmail.com>
2019-12-25 15:32:57 +0800

    list类中增加了lexListContent功能

--- value/MUAList.java
+++ value/MUAList.java
@@ -1,6 +1,9 @@
 package src.mua.value;
 
 import java.util.ArrayList;
+import java.util.Stack;
+
+import src.mua.dataSpace.*;
 
 //import java.util.ArrayList;
 
@@ -28,9 +31,130 @@ public class MUAList extends MUAValue{
 		return false;
 	}
 
-	public ArrayList<MUAValue> lexListContent()
+	public ArrayList<MUAValue> lexListContent(DataSpace glo, DataSpace loc)
 	{
 		ArrayList<MUAValue> res = new ArrayList<MUAValue>();
+		String cont = list.substring(1, list.length()-1).trim();
+		MUAList tmpList;
+		MUAWord tmpWord;
+
+		int off = 0;
+		int next = 0;
+		Stack<String> tmpStack = new Stack<String>();
+		String tmpSub = ""; // temporary substring
+
+		// skip all white spaces at beginning
+		while (cont.charAt(off) == ' ')
+			off++;
+		/* update next */
+		/* to next whitespace */
+		if (off < cont.length()) 
+		{
+			/* if it's a list */
+			if (cont.charAt(off) == '[') 
+			{
+				int k = off + 1;
+				tmpStack.push("[");
+				// find the matching ]
+				while (true) 
+				{
+					if (k < cont.length()) 
+					{
+						if (cont.charAt(k) == '[')
+							tmpStack.push("[");
+						else if (cont.charAt(k) == ']')
+							tmpStack.pop();
+					}
+					if (tmpStack.empty())
+						break;
+					else
+						k++;
+				}
+				if (k < cont.length() - 1)
+					next = k + 1; // add the entire list including [] into tokenList
+				else
+					next = cont.length();
+			} 
+			else
+				next = cont.indexOf(" ", off);
+		} 
+
+		while (next != -1 || off != cont.length()) 
+		{
+			if (next != -1) {
+				tmpSub = cont.substring(off, next);
+				tmpSub = tmpSub.trim();
+
+				if(tmpSub.charAt(0) == '[')
+				{
+					tmpList = new MUAList(tmpSub);
+					res.add(tmpList);
+				}
+				else
+				{
+					tmpWord = new MUAWord(tmpSub, glo, loc);
+					res.add(tmpWord);
+				}
+
+				/* update off */
+				/* to next not-whitespace */
+				off = next + 1;
+				if (off < cont.length()) {
+					while (cont.charAt(off) == ' ') {
+						off++;
+						if (off >= cont.length())
+							break;
+					}
+				}
+
+				/* update next */
+				/* to next whitespace */
+				if (off < cont.length()) 
+				{
+					/* if it's a list */
+					if (cont.charAt(off) == '[') 
+					{
+						int k = off + 1;
+						tmpStack.push("[");
+						// find the matching ]
+						while (true) {
+							if (k < cont.length()) 
+							{
+								if (cont.charAt(k) == '[')
+									tmpStack.push("[");
+								else if (cont.charAt(k) == ']')
+									tmpStack.pop();
+							}
+							if (tmpStack.empty())
+								break;
+							else
+								k++;
+						}
+						if (k < cont.length() - 1)
+							next = k + 1; // add the entire list including [] into res
+						else
+							next = cont.length();
+					} 
+					else
+						next = cont.indexOf(" ", off);
+				} 
+				else
+					break;
+			} 
+			else 
+			{ // the last token
+				tmpSub = cont.substring(off, cont.length());
+				if (tmpSub.charAt(0) == '[') {
+					tmpList = new MUAList(tmpSub);
+					res.add(tmpList);
+				} else 
+				{
+					tmpWord = new MUAWord(tmpSub, glo, loc);
+					res.add(tmpWord);
+				}
+				off = cont.length();
+			}
+		}
 
 		return res;
 	}
@@ -40,5 +164,33 @@ public class MUAList extends MUAValue{
 	public String toString() {
 		return list.substring(1, list.length()-1);
 	}
+
+	public static void main(String[] args) {
+		MUAList list = new MUAList("[[a [b]] c]");
+
+		MUAWord tmpWord;
+		MUAList tmpList;
+		String tmpSub;
+		DataSpace glo = new DataSpace();
+		DataSpace loc = new DataSpace();
+		ArrayList<MUAValue> res = list.lexListContent(glo, loc);
+
+		for (MUAValue val : res) {
+			if (val.getType() == "Word") {
+				tmpWord = (MUAWord) val;
+				tmpSub = tmpWord.toString();
+			} else {
+				tmpList = (MUAList) val;
+				tmpSub = tmpList.getList();
+			}
+
+			System.out.print(tmpSub + "$");
+		}
+
+
+		list.lexListContent(glo, loc);
+	}
 	
 }
+
+
