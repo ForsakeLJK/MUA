a6af35251d50c8ee58d6af7b5284ed20beb4771b
ForsakeLJK <forsakeljk@gmail.com>
2019-12-26 10:33:09 +0800

    加入first, butfirst, butlast在oplist，retcnt，operandcnt中。还未实现

--- parse/Parser.java
+++ parse/Parser.java
@@ -35,6 +35,7 @@ public class Parser {
 			"make", "thing", "erase", "isname", 
 			"sentence", "list", "join", "word",
 			"isempty", "islist",
+			"first", "butfirst", "butlast",
 			"print", "read", "readlist", "repeat",
 			"add", "sub", "mul", "div", "mod",
 			"eq", "gt", "lt",
@@ -502,6 +503,7 @@ public class Parser {
 					return 0;
 				else if(token.equals("thing")||token.equals("isname")||token.equals("read")||token.equals("readlist")
 						|| token.equals("isempty") || token.equals("islist")
+						|| token.equals("first") || token.equals("butfirst") || token.equals("butlast")
 						|| token.equals("sentence") || token.equals("list") || token.equals("join")|| token.equals("word")
 						|| token.equals("add")||token.equals("sub")||token.equals("mul")||token.equals("div")||token.equals("mod")
 						|| token.equals("eq")||token.equals("gt")||token.equals("lt")||token.equals("and")||token.equals("or")
@@ -569,6 +571,7 @@ public class Parser {
 				else if(token.equals("thing")||token.equals("erase")||token.equals("isname")
 					||token.equals("print")
 					||token.equals("isempty")||token.equals("islist")
+					||token.equals("first")||token.equals("butfirst")||token.equals("butlast")
 					||token.equals("not"))
 					return 1;
 				else if(token.equals("read")||token.equals("readlist"))
